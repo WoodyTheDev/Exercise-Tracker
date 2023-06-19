3d30ac734fe537d44dd30445e16159053f768b24
WoodyTheDev <holts@gmx.net>
2023-06-19 15:46:44 +0200

    removed old client and moved new client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,77 +1,23 @@
-
 <template>
-	<div>
-		<div class="main">
-			<h3>Todo List</h3>
-
-			<form class="form">
-				<input
-					class="input"
-					v-model="title"
-					type="text"
-					name="name"
-					placeholder="Enter todo"
-				/>
-				<br />
-				<input
-					class="input"
-					v-model="description"
-					type="text"
-					name="description"
-					placeholder="Enter Description"
-				/>
-				<br />
-				<button class="submit-button" @click="addTodo">Add Todo</button>
-			</form>
-			<div class="todo-container">
-				<ul>
-					<li v-for="(todo, i) in todos" :key="todo._id">
-						<div class="todo">
-							<span class="todo-name">{{ todo.title }}</span>
-							<span class="todo-description">{{ todo.description }}</span>
-						</div>
-						<button class="delete-btn" @click="removeTodo(todo, i)">
-							DELETE TODO
-						</button>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-import axios from "axios";
-export default {
-	name: "App",
-	data() {
-		return {
-			todos: [],
-			description: "",
-			title: "",
-		};
-	},
-	async mounted() {
-		const response = await axios.get("api/todoList/");
-		this.todos = response.data;
-	},
-	methods: {
-		async addTodo(e) {
-			e.preventDefault();
-			const response = await axios.post("api/todoList/", {
-				title: this.title,
-				description: this.description,
-			});
-			this.todos.push(response.data);
-			this.title = "";
-			this.description = "";
-		},
-		async removeTodo(item, i) {
-			await axios.delete("api/todoList/" + item._id);
-			this.todos.splice(i, 1);
-		},
-	},
-};
-</script>
+<script lang="ts">
+import { defineComponent } from "vue";
 
-<style></style>
+export default defineComponent({
+  name: "App",
+
+  components: {},
+
+  data() {
+    return {
+      //
+    };
+  },
+});
+</script>
